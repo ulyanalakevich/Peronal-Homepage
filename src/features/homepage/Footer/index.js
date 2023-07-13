f16f675798fd89ace82d76bf2c28c54d6c4326e5
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-13 15:41:07 +0200

    Component Footer created

---
+++ src/features/homepage/Footer/index.js
@@ -0,0 +1,20 @@
+import { email } from "../email";
+import { SocialIcons } from "./Socialicons";
+import { Address, EmailLink, EmailWrapper, LetsTalk, Paragraph, Wrapper } from "./styled";
+
+export const Footer = () => (
+  <Wrapper>
+    <LetsTalk>Let's talk!</LetsTalk>
+    <Address>
+      <EmailWrapper>
+        <EmailLink href={`mailto:${email}`}>{email}</EmailLink>
+      </EmailWrapper>
+      <Paragraph>
+        I’m always open to new projects whenever I have the time. If you have a&nbsp;
+        website, dashboard or mobile app in mind and need some help to&nbsp; make your
+        ideas come to life, feel free to conatct me🤞.
+      </Paragraph>
+      <SocialIcons/>
+    </Address>
+  </Wrapper>
+);
