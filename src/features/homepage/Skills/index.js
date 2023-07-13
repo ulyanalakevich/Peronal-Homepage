34ab1ab516c258c8fa1b563001590a496deba5dd
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-13 15:45:36 +0200

    Skills component created

---
+++ src/features/homepage/Skills/index.js
@@ -0,0 +1,14 @@
+import { Bullet, Item, List, Section, StyledHeader } from "./styled";
+
+export const Skills = ({title, skills}) => (
+    <Section>
+        <StyledHeader>{title}</StyledHeader>
+        <List>
+            {skills.map(skill => (
+                <Item key={skill}>
+                    <Bullet/>{skill}
+                </Item>
+            ))}
+        </List>
+    </Section>
+);
