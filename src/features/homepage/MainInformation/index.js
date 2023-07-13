d7fdbd5eb02b597d36e95c8c83a6f4b29782f174
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-13 15:42:35 +0200

    main information component created

---
+++ src/features/homepage/MainInformation/index.js
@@ -0,0 +1,19 @@
+import { Name, Image, ThisIs, Wrapper, Info, StyledButtonLink, ButtonIcon } from "./styled"
+import UlyanaLakevich from "./profile.jpeg"
+import { email } from "../email"
+export const MainInformation = () => {
+    return (
+        <Wrapper>
+            <Image src={UlyanaLakevich} alt="Ulyana Lakevich"/>
+            <div>
+                <ThisIs>This is</ThisIs>
+                <Name>Ulyana Lakevich</Name>
+                <Info>👨🏻‍💻I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities.</Info>
+                <StyledButtonLink href={`mailto:${email}`} title={email}>
+                    <ButtonIcon/>
+                    Hire me
+                </StyledButtonLink>
+            </div>
+        </Wrapper>
+    )
+}
