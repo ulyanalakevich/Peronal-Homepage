92589031eb3318d68d2c4ac17b5014d9ff83aea9
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-20 21:06:17 +0200

    text modified in Footer

--- src/features/homepage/Footer/index.js
+++ src/features/homepage/Footer/index.js
@@ -10,9 +10,8 @@ export const Footer = () => (
         <EmailLink href={`mailto:${email}`}>{email}</EmailLink>
       </EmailWrapper>
       <Paragraph>
-        I’m always open to new projects whenever I have the time. If you have a&nbsp;
-        website, dashboard or mobile app in mind and need some help to&nbsp; make your
-        ideas come to life, feel free to conatct me🤞.
+        I’m always open to new projects whenever I have the time. If you have a&nbsp;website, dashboard or mobile app in mind and need some help to&nbsp; make your
+        ideas come to life, feel free to contact me🤞.
       </Paragraph>
       <SocialIcons/>
     </Address>
